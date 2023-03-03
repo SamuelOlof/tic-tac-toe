f870cb00fcc2c2cd65bac80c9fcb8098cd3a48ce
SamuelOlof <samuelolofintoye@gmail.com>
2023-03-03 17:30:21 +0100

    checking something

--- src/index.js
+++ src/index.js
@@ -31,24 +31,27 @@ class Square extends React.Component {
       const status = 'Next player: X';
   
       return (
-        // <div>
-        //   <div className="status">{status}</div>
-        //   <div className="board-row">
-        //     {this.renderSquare(0)}
-        //     {this.renderSquare(1)}
-        //     {this.renderSquare(2)}
-        //   </div>
-        //   <div className="board-row">
-        //     {this.renderSquare(3)}
-        //     {this.renderSquare(4)}
-        //     {this.renderSquare(5)}
-        //   </div>
-        //   <div className="board-row">
-        //     {this.renderSquare(6)}
-        //     {this.renderSquare(7)}
-        //     {this.renderSquare(8)}
-        //   </div>
-        // </div>
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div>
+            <p>status</p>
+          </div>
+        </div>
       );
     }
   }
